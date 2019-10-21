319225942d7ba29f4348e169a1355e64aa353429
IMF <ipmafr@gmail.com>
2019-10-21 16:53:31 +0300

    Not pretty but functional

--- KorkeusDEMOCPP/KorkeusDEMOCPP/KorkeusDEMOCPP.cpp
+++ KorkeusDEMOCPP/KorkeusDEMOCPP/KorkeusDEMOCPP.cpp
@@ -10,6 +10,11 @@
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+bool isInitializationDone = false;
+
+// Global classes
+MapRGBData rgb;
+MapMetadata meta;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -18,41 +23,41 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_KORKEUSDEMOCPP, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_KORKEUSDEMOCPP, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_KORKEUSDEMOCPP));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_KORKEUSDEMOCPP));
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -64,23 +69,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_KORKEUSDEMOCPP));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_KORKEUSDEMOCPP);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_KORKEUSDEMOCPP));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_KORKEUSDEMOCPP);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -95,20 +100,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -123,58 +128,83 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_CREATE:
+		isInitializationDone = FileIntoRGB(rgb, meta);
+		break;
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		HDC hMemDC = CreateCompatibleDC(hdc);
+		if (isInitializationDone == true) {
+			int mapWidth = meta.mapwidth;
+			int mapHeight = meta.mapheight;
+			HBITMAP hBmp = ::CreateCompatibleBitmap(hdc, mapWidth, mapHeight);
+			BitmapData mapBit(hdc, hBmp, rgb.rgbdata, mapWidth, mapHeight);
+
+			int asdf = mapBit.DIBtoDDB();
+			if (asdf == 0) {
+				::MessageBox(NULL, __T("Failed to set DIB."), __T("Error"), MB_OK);
+				return NULL;
+			}
+			HBITMAP holdmap = (HBITMAP)::SelectObject(hMemDC, hBmp);
+			if (holdmap == NULL) {
+				::MessageBox(NULL, __T("Failed to SelectObject."), __T("Error"), MB_OK);
+				return NULL;
+			}
+			BOOL testi = ::BitBlt(hdc, 0, 0, mapWidth, mapHeight, hMemDC, 0, 0, SRCCOPY);
+			if (!testi) {
+				::MessageBox(NULL, __T("Failed to BitBlt."), __T("Error"), MB_OK);
+				return NULL;
+			}
+		}
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
--- KorkeusDEMOCPP/KorkeusDEMOCPP/KorkeusDEMOCPP.h
+++ KorkeusDEMOCPP/KorkeusDEMOCPP/KorkeusDEMOCPP.h
@@ -1,3 +1,155 @@
+// Code for visualizing ascii elevation data
+
 #pragma once
 
 #include "resource.h"
+#include "framework.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Holds metadata about the map
+class MapMetadata {
+public:
+	int mapwidth = 0;
+	int mapheight = 0;
+	int nodatavalue = 0;
+
+	long MapSize() { return (mapwidth * mapheight); };
+};
+
+// Holds an array of RGB values
+class MapRGBData {
+public:
+	unsigned char* rgbdata;
+	void Initialize(int koko) { rgbdata = new unsigned char[koko * 3]; };
+	~MapRGBData() { delete[] rgbdata; };
+};
+
+// Holds a necessary bitmap header
+// Also has function for outputting picture
+class BitmapData {
+	int width, height;
+	void* pMap;
+	HBITMAP bitmapHandle;
+	HDC hdc;
+
+public:
+	BITMAPINFOHEADER mapHeader;
+	BITMAPINFO mapInfo;
+
+	BitmapData(HDC thdc, HBITMAP thandle, void* tpMap, int twidth, int theight) {
+		width = twidth;
+		height = theight;
+		pMap = tpMap;
+		hdc = thdc;
+		bitmapHandle = thandle;
+
+		mapHeader = {
+		   sizeof(BITMAPINFOHEADER),	//bV5Size
+		   width,		//bV5Width
+		   -height,		//bV5Height
+		   1,			//bV5Planes
+		   24,			//bV5BitCount
+		   BI_RGB,		//bV5Compression
+		   0,			//bV5SizeImage
+		   0,			//bV5XPelsPerMeter
+		   0,			//bV5YPelsPerMeter
+		   0,			//bV5ClrUsed
+		   0,			//bV5ClrImportant
+		};
+
+		mapInfo = {
+			mapHeader
+		};
+	};
+
+	int DIBtoDDB() {
+		int asdf = SetDIBits(hdc, bitmapHandle, 1, height, pMap, &mapInfo, DIB_PAL_COLORS);
+		if (asdf == 0) {
+			::MessageBox(NULL, __T("Failed to set DIBits."), __T("Error"), MB_OK);
+			return NULL;
+		}
+		else return asdf;
+	};
+};
+
+// Calculates elevation data into RGB values
+void CalculateRGBValues(vector<double>& maparray, unsigned char*& rgbarray, MapMetadata& metadata) {
+
+	// Calculate RGB values based on the max range of elevations
+	double minValue = *min_element(maparray.begin(), maparray.end());
+	double maxValue = *max_element(maparray.begin(), maparray.end());
+	double mapRange = maxValue - minValue;
+
+	int n;
+	for (int i = 0; i < metadata.MapSize(); i++) {
+		n = i * 3;
+		rgbarray[n] = 0;
+		rgbarray[n + 1] = (unsigned char)round(((maparray[i] - minValue) / mapRange) * 255);
+		rgbarray[n + 2] = 0;
+	}
+};
+
+// Main function for opening and handling the file
+// From string to tokenized float to RGB
+// Target file hardcoded, for now
+bool FileIntoRGB(MapRGBData& mapRGB, MapMetadata& meta) {
+
+	// Open file
+	ifstream mapfile(".\\files\\L3324.asc");
+	if (mapfile.is_open()) {
+
+		// String stream
+		stringstream mapstring;
+		mapstring << mapfile.rdbuf();
+
+		// Close file
+		mapfile.close();
+
+		// Read metadata
+		// Horrible hack version
+		std::string* ignore = new std::string;
+		for (int i = 0; i < 13; i++) {
+			switch (i) {
+			case 1:
+				mapstring >> meta.mapwidth;
+				break;
+			case 3:
+				mapstring >> meta.mapheight;
+				break;
+			case 11:
+				mapstring >> meta.nodatavalue;
+				break;
+			default:
+				mapstring >> *ignore;
+			}
+		}
+		delete ignore;
+
+		// Tokenization + conversion to float
+		// Quite hacky
+		vector<double> mapElevationData;
+		char* temp = new char[10];
+
+		for (int i = 0; i < meta.MapSize(); i++) {
+			mapstring >> temp;
+			mapElevationData.push_back(atof(temp));
+		}
+		delete[] temp;
+		mapstring.str("");
+
+		// Calculate RGB values
+		mapRGB.Initialize(meta.MapSize());
+		CalculateRGBValues(mapElevationData, mapRGB.rgbdata, meta);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+};
